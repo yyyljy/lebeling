f47b3b8c986ae1b171ee9ee8fc8be12583981476
yyyljy <yyyljy@gmail.com>
2020-07-04 17:00:47 +0900

    change url

--- web_leb/urls.py
+++ web_leb/urls.py
@@ -18,10 +18,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('mainpage.urls')),
     path('accounts/', include('accounts.urls')),
-    path('mainpage/', include('mainpage.urls')),
     path('boxing/', include('boxing.urls')),
     path('taging/', include('taging.urls')),
     path('ranking/', include('ranking.urls')),
